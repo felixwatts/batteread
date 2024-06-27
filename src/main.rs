3073d3a1efd3d7eda284a97be8ae554bb080b890
Felix Watts <felixwatts@gmail.com>
2024-06-27 11:19:28 +0100

    builds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use bluetooth_serial_port_async::{BtProtocol, BtSocket};
+use tokio_modbus::prelude::*;
+
+const DEVICE_NAME: &'static str = "HC_BT1234";
+const REG_STATE_OF_CHARGE: u16 = 0x0;
+const LEN_STATE_OF_CHARGE: u16 = 1;
+
+#[tokio::main]
+async fn main() {
+    let devices = bluetooth_serial_port_async::scan_devices(std::time::Duration::from_secs(20)).unwrap();
+    if devices.len() == 0 {
+        panic!("No devices found");
+    }
+
+    println!("Found bluetooth devices {:?}", devices);
+
+    let device = devices.iter().find(|d| d.name == DEVICE_NAME).expect("BMS device not found");
+
+    let mut socket = BtSocket::new(BtProtocol::RFCOMM).unwrap();
+    socket.connect(device.addr).unwrap();
+
+    let stream = socket.get_stream();
+
+    // let slave = Slave(0x01);
+    let mut ctx = rtu::attach(stream);
+
+    println!("Reading a sensor value");
+    let rsp = ctx.read_holding_registers(REG_STATE_OF_CHARGE, LEN_STATE_OF_CHARGE).await.unwrap().unwrap();
+    println!("Sensor value is: {rsp:?}");
+
+    println!("Disconnecting");
+    ctx.disconnect().await.unwrap().unwrap();
+}
+
 // use bluetooth_serial_port::{BtProtocol, BtSocket};
 // use std::io::{Read, Write};
 // use std::time;
@@ -39,78 +73,106 @@
     // // loop { ... poll events and wait for socket to be readable/writable ... }
 // }
 
-use btleplug::api::{Central, CharPropFlags, Peripheral, ScanFilter};
-use btleplug::api::Manager as _;
-use btleplug::platform::Manager;
-use tokio_modbus::prelude::*;
-use tokio::time::{sleep, Duration};
-use std::sync::Arc;
+// struct BtSocketEventSource{
+//     wrapped: BtSocket
+// }
 
-#[tokio::main]
-async fn main() {
-    // Initialize the Bluetooth manager
-    let manager = Manager::new().await.unwrap();
+// impl mio::event::Source for BtSocketEventSource{
+//     fn register(
+//         &mut self,
+//         registry: &mio::Registry,
+//         token: Token,
+//         interests: Interest,
+//     ) -> std::io::Result<()> {
+//         self.wrapped.register(registry, token, interests)
+//     }
 
-    // Get the first Bluetooth adapter
-    let adapters = manager.adapters().await.unwrap();
-    let central = adapters.into_iter().nth(0).expect("No Bluetooth adapter found");
+//     fn reregister(
+//         &mut self,
+//         registry: &mio::Registry,
+//         token: Token,
+//         interests: Interest,
+//     ) -> std::io::Result<()> {
+//         todo!()
+//     }
 
-    // Start scanning for devices
-    central.start_scan(ScanFilter::default()).await.unwrap();
-    sleep(Duration::from_secs(15)).await; // Allow some time to discover devices
+//     fn deregister(&mut self, registry: &mio::Registry) -> std::io::Result<()> {
+//         todo!()
+//     }
+// }
 
-    // Find the specified device by name
-    let device_name = "BT_HC6172";
-    let peripherals = central.peripherals().await.unwrap();
+// use btleplug::api::{Central, CharPropFlags, Peripheral, ScanFilter};
+// use btleplug::api::Manager as _;
+// use btleplug::platform::Manager;
+// use tokio_modbus::prelude::*;
+// use tokio::time::{sleep, Duration};
+// use std::sync::Arc;
 
-    let peripheral = find_peripheral(peripherals, device_name).await.expect("Bluetooth device not found");
+// #[tokio::main]
+// async fn main() {
+//     // Initialize the Bluetooth manager
+//     let manager = Manager::new().await.unwrap();
 
-    // Connect to the device
-    peripheral.connect().await.unwrap();
-    peripheral.discover_services().await.unwrap();
-    println!("Connected to Bluetooth device.");
+//     // Get the first Bluetooth adapter
+//     let adapters = manager.adapters().await.unwrap();
+//     let central = adapters.into_iter().nth(0).expect("No Bluetooth adapter found");
 
-    // // Setup Modbus client
+//     // Start scanning for devices
+//     central.start_scan(ScanFilter::default()).await.unwrap();
+//     sleep(Duration::from_secs(15)).await; // Allow some time to discover devices
 
-    for c in peripheral.characteristics().into_iter() {
-        let uuid = c.uuid;
-        let props = c.properties;
-        println!("{uuid} {props:?}");
+//     // Find the specified device by name
+//     let device_name = "BT_HC6172";
+//     let peripherals = central.peripherals().await.unwrap();
 
-        if props.contains(CharPropFlags::READ) {
-            let read_result = peripheral.read(&c).await;
-            match read_result {
-                Ok(data) => println!("{uuid} = {data:?}"),
-                Err(e) => println!("{uuid} error: {e}"),
-            }
-        }
-    }
+//     let peripheral = find_peripheral(peripherals, device_name).await.expect("Bluetooth device not found");
 
-    // 00001534-1212-efde-1523-785feabcd123 READ
-    // 00002a26-0000-1000-8000-00805f9b34fb READ
-    // 00002a29-0000-1000-8000-00805f9b34fb READ
+//     // Connect to the device
+//     peripheral.connect().await.unwrap();
+//     peripheral.discover_services().await.unwrap();
+//     println!("Connected to Bluetooth device.");
 
-    // tokio_modbus::prelude::tcp::connect(peripheral);
-    // let socket_addr = "192.168.0.100:502"; // Replace with your Modbus server address
-    // let client = tcp::connect(socket_addr).await.unwrap();
-    // let client = Arc::new(client);
+//     // // Setup Modbus client
 
-    // // Read Modbus data in a loop
-    // loop {
-    //     let read = client.read_holding_registers(0x00, 10).await.unwrap();
-    //     println!("Read data: {:?}", read);
+//     for c in peripheral.characteristics().into_iter() {
+//         let uuid = c.uuid;
+//         let props = c.properties;
+//         println!("{uuid} {props:?}");
 
-    //     sleep(Duration::from_secs(5)).await;
-    // }
-}
+//         if props.contains(CharPropFlags::READ) {
+//             let read_result = peripheral.read(&c).await;
+//             match read_result {
+//                 Ok(data) => println!("{uuid} = {data:?}"),
+//                 Err(e) => println!("{uuid} error: {e}"),
+//             }
+//         }
+//     }
 
-async fn find_peripheral<T>(peripherals: Vec<T>, device_name: &'static str) -> Option<T> where T: Peripheral {
-    for p in peripherals.into_iter() {
-        let local_name = p.properties().await.ok().flatten().map(|p| p.local_name).flatten();
-        match local_name {
-            Some(name) if name == device_name => return Some(p),
-            _ => {}
-        }
-    }
-    return None
-}
+//     // 00001534-1212-efde-1523-785feabcd123 READ : Device Firmware Version
+//     // 00002a26-0000-1000-8000-00805f9b34fb READ : Firmware Revision String
+//     // 00002a29-0000-1000-8000-00805f9b34fb READ : Manufacturer Name String
+
+//     // tokio_modbus::prelude::tcp::connect(peripheral);
+//     // let socket_addr = "192.168.0.100:502"; // Replace with your Modbus server address
+//     // let client = tcp::connect(socket_addr).await.unwrap();
+//     // let client = Arc::new(client);
+
+//     // // Read Modbus data in a loop
+//     // loop {
+//     //     let read = client.read_holding_registers(0x00, 10).await.unwrap();
+//     //     println!("Read data: {:?}", read);
+
+//     //     sleep(Duration::from_secs(5)).await;
+//     // }
+// }
+
+// async fn find_peripheral<T>(peripherals: Vec<T>, device_name: &'static str) -> Option<T> where T: Peripheral {
+//     for p in peripherals.into_iter() {
+//         let local_name = p.properties().await.ok().flatten().map(|p| p.local_name).flatten();
+//         match local_name {
+//             Some(name) if name == device_name => return Some(p),
+//             _ => {}
+//         }
+//     }
+//     return None
+// }
