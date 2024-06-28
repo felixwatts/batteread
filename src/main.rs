e940c0e9ef7fae5dc33ade9df0514c6d7643850f
Felix Watts <felix@provenance.org>
2024-06-28 16:38:14 +0100

    debug

--- src/main.rs
+++ src/main.rs
@@ -135,9 +135,9 @@ async fn main() {
     // Start scanning for devices
     central.start_scan(ScanFilter::default()).await.unwrap();
 
-    println!("Scanning for 60s");
+    println!("Scanning for 30s");
 
-    sleep(Duration::from_secs(60)).await; // Allow some time to discover devices
+    sleep(Duration::from_secs(30)).await; // Allow some time to discover devices
 
     println!("Finished scanning");
 
